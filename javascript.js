267b3f46fdc9f67563ad9bdc828a6a35cd056420
Cheri Haggard <cheri@MATHmaniaRobotics.com>
2018-11-29 11:46:41 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//Warning button
+function myFunction() {
+    alert("Warning! The most delicious cupcakes!");
+}
+
+//3 Cupcake popups
+function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+}
+
+function myFunction2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
+
+function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+}
